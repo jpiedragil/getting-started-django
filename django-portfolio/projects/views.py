58641ad778fce0c9389e5942e3e363e88fcf14eb
Javier Piedragil <jpiedragil@gmailcom>
2020-12-21 19:31:17 -0600

    📦 NEW: Create a View
    - Add and configure a simple view.

--- django-portfolio/projects/views.py
+++ django-portfolio/projects/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def project_list(request):
+
+    return HttpResponse("<h1>Hello everyone!</h1>")
