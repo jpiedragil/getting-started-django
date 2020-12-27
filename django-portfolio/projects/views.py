fa0ce44d78ea55ea54cc4287d2a5cb46ff0e10db
Javier Piedragil <jpiedragil@gmailcom>
2020-12-27 01:31:18 -0600

    📦 NEW: Build Views
    - Add view to retrieve all projects from the db.

--- django-portfolio/projects/views.py
+++ django-portfolio/projects/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
+from projects.models import Project
+
 
 # Create your views here.
 def project_list(request):
 
     return render(request, 'projects/index.html')
+
+
+def all_projects(request):
+
+    # Query the db to return all project objects
+    projects = Project.objects.all()
+    print(projects)
+
+    return render(request, 'all_projects.html', {'projects': projects})
